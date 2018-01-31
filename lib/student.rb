121a8a1853198fabd8533aef86d42a35ef3ebc0d
Jordan Kline <jordan.kline@gmail.com>
2018-01-31 18:02:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,14 @@ require_relative "../config/environment.rb"
 
 class Student
 attr_accessor :name, :grade
+attr_reader :id
 
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
   end
+
+  
 
 end
