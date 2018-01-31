277218f1da4b1e369f8849d5670035a8c6a97ff4
Jordan Kline <jordan.kline@gmail.com>
2018-01-31 18:09:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,12 @@ attr_reader :id
   end
 
   def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
 
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
   def self.create
